00e0dd45d10de2e1409dbc9277f957b739cc7919
Maria <92776600+git21git@users.noreply.github.com>
2026-06-19 17:06:01 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/practice_13_react/'
+  homepage: "https://git21git.github.io/practice_13_react/"
 })
